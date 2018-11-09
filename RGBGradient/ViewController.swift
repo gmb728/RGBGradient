54591182680bceb960e617945e37e91f40694552
Chang Sophia <you@example.com>
2018-11-09 13:35:35 +0800

    Initial Commit

---
+++ RGBGradient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGBGradient
+//
+//  Created by Chang Sophia on 11/9/18.
+//  Copyright © 2018 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
